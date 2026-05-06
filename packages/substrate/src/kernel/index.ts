180699f849002b8f49ab6daebbd0250a5e8f8aa3
Gurdas Nijor <nijor22@gmail.com>
2026-05-05 18:22:36 -0700

    Separate substrate state-store and read-model modules

--- packages/substrate/src/kernel/index.ts
+++ packages/substrate/src/kernel/index.ts
@@ -7,18 +7,18 @@
 
 export * from "../descriptors/index.ts"
 export * from "../schema/index.ts"
-export * from "../projection.ts"
-export * from "../stream.ts"
+export * from "../read-models/projection.ts"
+export * from "../state-store/stream.ts"
 export * from "../state-machine.ts"
 export {
   completeRun as completeRunEffect,
   failRun as failRunEffect,
 } from "../schema/state-machine.ts"
 export * from "../producer.ts"
-export * from "../projection/ready-work.ts"
+export * from "../read-models/ready-work.ts"
 export * from "../operator.ts"
 export * from "../operator-errors.ts"
-export * from "../retained-records.ts"
+export * from "../state-store/retained-records.ts"
 export * from "../waits.ts"
 export * from "../subscribers.ts"
 export * from "../id-gen.ts"
